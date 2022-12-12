cc3e8413f7deb8a8dc8fe778835a81b33c9e7c0a
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-12 17:57:55 -0500

    fetching from a reconstructed url

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,22 @@
+// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
 
-var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4f112ad8f388d7d13afdcbf2472fed94"
+//  var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94"
+
+
+var cityName = $("#cityNameInput").val();
+//var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94";
+
+
+$(function() {
+     
+  $(".btn").on("click", function(event) {
+ 
+    var cityName = $("#cityNameInput").val();
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=4f112ad8f388d7d13afdcbf2472fed94";
+
+
+
+
 
 fetch(requestUrl)
   .then(function (response) {
@@ -11,7 +28,10 @@ fetch(requestUrl)
   .catch(function (error) {
     console.log(error);
   });
+  
 
-
+})
+})
 //   4f112ad8f388d7d13afdcbf2472fed94
 // 4b03013a438c0f5c7a7400d32f63cd69
+

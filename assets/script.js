600cc0e3ca0b707e9bd017e0944d3d36fcf9e32e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-13 00:41:17 -0500

    tests plus formating

--- assets/script.js
+++ assets/script.js
@@ -4,53 +4,50 @@
 
 var apiKey = "4f112ad8f388d7d13afdcbf2472fed94";
 
-$(function() {
-  
-  $(".btn").on("click", function(event) {
+$(function () {
+
+  $(".btn").on("click", function (event) {
     event.preventDefault();
     var cityName = $("#cityNameInput").val();
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
-    
-    
+
+
     fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    
-    })
-    .then(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        var latitude = (data[i].lat);
-        console.log(latitude);
-        var longitude = (data[i].lon);
-        console.log(longitude);
-        var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-console.log(theUrl);
+      .then(function (response) {
+        return response.json();
 
-fetch(theUrl)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-})
-.catch(function (error) {
-  console.log(error);
+      })
+      .then(function (data) {
+        console.log(data)
+        for (var i = 0; i < data.length; i++) {
+          var latitude = (data[i].lat);
+          // console.log(latitude);
+          var longitude = (data[i].lon);
+          // console.log(longitude);
+          var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+          //  console.log(theUrl);
+
+          fetch(theUrl)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
+              for (var i = 0; i < data.length; i++) {
+                console.log(data[i].main.weather.icon);
+              }
+
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        }
+      })
+
+  })
+
 });
-      }
-    })
-    
-    // fetch(theUrl)
-    // .then(function (response) {
-    //   return response.json();
-    //   console.log(theUrl);
-    //   })
-     
 
-   })
-  // .catch(function (error) {
-    // console.log(error);
-  });
-//  });
 
 
 

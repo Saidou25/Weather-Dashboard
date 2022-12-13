dc68a655ce430d5ae62fc582bb500d86f601398c
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-13 09:30:59 -0500

    added dayjs, day and time print in the web card

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 // `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
 
 //  var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94"
-
+var currentDay = dayjs().format(" M/D/YYYY");
+// console.log(currentDay);
 var apiKey = "4f112ad8f388d7d13afdcbf2472fed94";
 
 $(function () {
@@ -11,27 +12,29 @@ $(function () {
     var cityName = $("#cityNameInput").val();
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
 
-
+    
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-
-      })
-      .then(function (data) {
-        console.log(data)
-        for (var i = 0; i < data.length; i++) {
-          var latitude = (data[i].lat);
-          // console.log(latitude);
-          var longitude = (data[i].lon);
-          // console.log(longitude);
-          var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-          //  console.log(theUrl);
-
-          fetch(theUrl)
-            .then(function (response) {
-              return response.json();
-            })
-            .then(function (data) {
+    .then(function (response) {
+      return response.json();
+      
+    })
+    .then(function (data) {
+      console.log(data)
+      for (var i = 0; i < data.length; i++) {
+        var cityReturn = (data[i].name)
+        var latitude = (data[i].lat);
+        // console.log(latitude);
+        var longitude = (data[i].lon);
+        // console.log(longitude);
+        var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+        //  console.log(theUrl);
+        $("#city-date").append(cityReturn + (currentDay))
+        
+        fetch(theUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
               console.log(data);
               for (var i = 0; i < data.length; i++) {
                 console.log(data[i].main.weather.icon);
@@ -45,7 +48,7 @@ $(function () {
       })
 
   })
-
+ 
 });
 
 

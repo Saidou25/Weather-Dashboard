9632345058a5dd61f8f7f61e561290b257b68503
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-12 23:32:06 -0500

    added a second fetch for the new url

--- assets/script.js
+++ assets/script.js
@@ -5,33 +5,45 @@
 var apiKey = "4f112ad8f388d7d13afdcbf2472fed94";
 
 $(function() {
-     
+  
   $(".btn").on("click", function(event) {
     event.preventDefault();
     var cityName = $("#cityNameInput").val();
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
+    
+    
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    
+    })
+    .then(function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var latitude = (data[i].lat);
+        console.log(latitude);
+        var longitude = (data[i].lon);
+        console.log(longitude);
+        var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+console.log(theUrl);
 
-
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  
-  })
-  .then(function(data) {
-    for (var i = 0; i < data.length; i++) {
-      var lat = (data[i].lat);
-       console.log(lat);
-      var lon = (data[i].lon);
-       console.log(lon);
-      // var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
-  }
-      })
-
-      fetch(theUrl)
-      .then(function (response) {
-        return response.json();
-      console.log(theUrl);
-      })
+fetch(theUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (error) {
+  console.log(error);
+});
+      }
+    })
+    
+    // fetch(theUrl)
+    // .then(function (response) {
+    //   return response.json();
+    //   console.log(theUrl);
+    //   })
      
 
    })

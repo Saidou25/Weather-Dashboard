e5e94978045f35d5174692a96672550902e0d153
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-12 23:11:50 -0500

    added the loop to only extract lat and lon datas

--- assets/script.js
+++ assets/script.js
@@ -2,36 +2,46 @@
 
 //  var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94"
 
-
-var cityName = $("#cityNameInput").val();
-//var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94";
-
+var apiKey = "4f112ad8f388d7d13afdcbf2472fed94";
 
 $(function() {
      
   $(".btn").on("click", function(event) {
- 
+    event.preventDefault();
     var cityName = $("#cityNameInput").val();
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=4f112ad8f388d7d13afdcbf2472fed94";
-
-
-
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
 
 
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
   
+  })
+  .then(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var lat = (data[i].lat);
+       console.log(lat);
+      var lon = (data[i].lon);
+       console.log(lon);
+      // var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  }
+      })
 
-})
-})
-//   4f112ad8f388d7d13afdcbf2472fed94
-// 4b03013a438c0f5c7a7400d32f63cd69
+      fetch(theUrl)
+      .then(function (response) {
+        return response.json();
+      console.log(theUrl);
+      })
+     
+
+   })
+  // .catch(function (error) {
+    // console.log(error);
+  });
+//  });
+
+
+
+
+
 

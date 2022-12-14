2a3aaec22f2492a14bafc3517b1d3487c5ed707a
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-14 00:12:21 -0500

    fixed today url and added imperial. generated card id displayed as expected

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
 // `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-
+// http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 //  var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=raleigh&appid=4f112ad8f388d7d13afdcbf2472fed94"
-var currentDay = dayjs().format(" M/D/YYYY");
-// console.log(currentDay);
+
+// var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+
+
+
+
+
+var currentDay = dayjs().format(" (M/D/YYYY)");
 var apiKey = "4f112ad8f388d7d13afdcbf2472fed94";
 
 $(function () {
@@ -12,48 +18,74 @@ $(function () {
     var cityName = $("#cityNameInput").val();
     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey;
 
-    
+
     fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-      
-    })
-    .then(function (data) {
-      console.log(data)
-      for (var i = 0; i < data.length; i++) {
-        var cityReturn = (data[i].name)
-        var latitude = (data[i].lat);
-        // console.log(latitude);
-        var longitude = (data[i].lon);
-        // console.log(longitude);
-        var theUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-        //  console.log(theUrl);
-        $("#city-date").append(cityReturn + (currentDay))
-        
-        fetch(theUrl)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-              console.log(data);
-              for (var i = 0; i < data.length; i++) {
-                console.log(data[i].main.weather.icon);
+      .then(function (response) {
+        return response.json();
+
+      })
+      .then(function (data) {
+        $()
+        for (var i = 0; i < data.length; i++) {
+          var cityReturn = (data[i].name)
+          var latitude = (data[i].lat);
+          var longitude = (data[i].lon);
+          var todayUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+
+          fetch(todayUrl)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data)
+              $("today-card").empty();
+              // for (var i = 0; i < data.list.length; i++) {
+
+
+                var todayHumidity = (data.main.humidity);
+                var todayTemp = (data.main.temp);
+                var todayWind = (data.wind.speed);
+                // var todayTimeZone = (data.city.timezone);
+
+
+                var col = $("<div>").addClass("col-12");
+                var card = $("<div>").addClass("card");
+                var cardBody = $("<div>").addClass("card-body");
+                var cardHumidity = $("<div>").addClass("card-humidity");
+                var cardTemp = $("<div>").addClass("card-temp");
+                var cardWind = $("<div>").addClass("card-wind");
+
+                cardHumidity.text(todayHumidity + " %");
+                cardTemp.text(todayTemp + " F");
+                cardWind.text(todayWind + " MPH");
+
+                cardBody
+                  .append(cityReturn + currentDay)
+                  .append(cardTemp)
+                  .append(cardWind)
+                  .append(cardHumidity);
+
+                card
+                  .append(cardBody);
+
+                col
+                  .append(card);
+
+                $("#today-card").append(col);
               }
 
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+            )
+            // .catch(function (error) {
+            //   console.log(error);
+            // });
         }
       })
-
   })
- 
 });
 
 
-
-
-
-
+// var todayHumidity = (data.list[1].main.humidity);
+// var todayTemp = (data.list[1].main.temp);
+// var todayWind = (data.list[1].wind.speed);
+// var todayTimeZone = (data.city.timezone);
 
